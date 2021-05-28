3a1e5df6625b1602dbe4a8a7255f5bbc2aa27e82
jcarlosaguirre <juancarlos.aguirrecortes@gmail.com>
2021-05-28 22:04:58 +0200

    filled some information

--- js/vue.js
+++ js/vue.js
@@ -5,10 +5,17 @@ const vueApp = {
         return{
 
             links: [
-                { title: "Home", disabled: false },
-                { title: "About me", disabled: false },
-                { title: "Skills", disabled: false },
-                { title: "Projects", disabled: false },
+                { title: "Home", value: 0, disabled: false },
+                { title: "About me", value: 1, disabled: false },
+                { title: "Skills", value: 2, disabled: false },
+                { title: "Projects", value: 3, disabled: false },
+            ],
+
+            iconIndex: 0,
+
+            icons: [
+                'fa-home',
+                'fa-user'
             ],
 
             // Active or deactivate home
@@ -21,8 +28,21 @@ const vueApp = {
                             "It could also be that you ended up here by accident, but take a look and maybe you will be interested in me (in a professional way, of course).",
                     section1Title: "Know my skills",
                     section1Text: "Here you will find which technologies I am in, which ones I have basic knowledge on and which ones make me curious to learn.",
-                    section2Title: "Visit my Github repository",
-                    section2Text: "and find in which projects I'm working on (maybe none right now, at the moment).",
+                    section2Title: "Visit my Github page",
+                    section2Text: "and find in which projects I'm working on! (maybe none right now, at the moment).",
+                },
+                about:{
+                    title:  "A little about me",
+                    text1:  "Wanting to work in this virtual world that is the internet begins with being a videogames player. " +
+                            "Videogames have always caught my attention and I wanted to enter the world of programming because of them." + "\n\n" +
+                            "Right now I am studying <b>web development</b>, identifying myself as a full-stack developer. " + 
+                            "Javascript is my <b>main stack</b>, but I also have knowledge of other languages ​​and I would like to continue learning different technologies" + "\n\n" +
+                            "My intention is to <b>continue learning as much as I can</b>, whether in a work environment, study or combining both. " +
+                            "I consider myself an organized person, self-taught and with whom it is easy to work so, if you are <b>looking for someone for your team</b>, here I leave my contact information:",
+                    section1Title: "Know my skills",
+                    section1Text: "Here you will find which technologies I am in, which ones I have basic knowledge on and which ones make me curious to learn.",
+                    section2Title: "Hobbies",
+                    section2Text: "<ul class='p-3'><li class='py-1'>Playing the guitar</li><li class='py-1'>Videogames</li><li class='py-1'>I love listening to music</li><li class='py-1'>To have ideas that I never develop</li></ul>",
                 }
             },
 
@@ -32,6 +52,7 @@ const vueApp = {
                 tellMeMore: "Tell me more!",
                 bringMeThere: "Bring me there!",
                 takeALook: "Let's take a look",
+                contactMe: "Contact info"
             },
 
             // Decorative section separators (filled in 'mounted' hook)
@@ -71,54 +92,35 @@ const vueApp = {
     },
     methods:{
 
-        // Toggle menu
-        showMenu(){
-            
-            // Change menu state
-            this.openMenu = !this.openMenu;
-
-            let i = 1;
-
-            // Delay for other links
-            let interval = setInterval(function () {
-
-                // Clean interval when no more links available
-                if (i == links.length - 1) clearInterval(interval);
-
-                links[i].classList.toggle("hidden");
-                i++;
-
-            }, 200);
-
-            // Toggle first element instantly
-            links[0].classList.toggle("hidden");
-        },
-
         // Ahow a section or another
-        showSection( section ){
+        setSection( value ){
 
-            switch (section) {
+            switch (value) {
 
                 case 0:
-                    this.homeSection = true;
-                    this.projectsSection = false;
-                    this.aboutSection = false;
+                    this.section = "Home";
                     break;
 
                 case 1:
-                    this.homeSection = false;
-                    this.projectsSection = false;
-                    this.aboutSection = true;
+                    this.section = "About me";
                     break;
                     
                 case 2:
-                    this.homeSection = false;
-                    this.aboutSection = false;
-                    this.projectsSection = true;
+                    this.section = "Skills";
+                    break;
+                    
+                case 3:
+                    this.section = "Projects";
                     break;
             }
 
-            this.animateSeparator(section);
+            this.icons.forEach(icon => {
+                console.log( icon );
+                document.getElementById("header-icon").classList.remove( icon )
+            })
+            
+            document.getElementById("header-icon").classList.add( this.icons[ value ] )
+            
         },
 
         // Animate a delimiter or another based on chosen section
