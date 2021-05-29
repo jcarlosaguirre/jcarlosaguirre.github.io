8d2d031b3701b947fa47bf66152f1f28667a0234
jcarlosaguirre <juancarlos.aguirrecortes@gmail.com>
2021-05-29 16:56:19 +0200

    About section done

--- js/vue.js
+++ js/vue.js
@@ -15,7 +15,8 @@ const vueApp = {
 
             icons: [
                 'fa-home',
-                'fa-user'
+                'fa-user',
+                'fa-layer-group'
             ],
 
             // Active or deactivate home
@@ -25,33 +26,51 @@ const vueApp = {
                 home:{
                     title:  "Welcome to my resume!",
                     text1:  "This site is open to anyone looking for some information about me." + "\n" +
-                            "It could also be that you ended up here by accident, but take a look and maybe you will be interested in me (in a professional way, of course).",
+                            "It could also be that you ended up here by accident, but take a look and maybe you will be interested in me <b>(in a professional way, of course)</b>.",
                     section1Title: "Know my skills",
                     section1Text: "Here you will find which technologies I am in, which ones I have basic knowledge on and which ones make me curious to learn.",
                     section2Title: "Visit my Github page",
                     section2Text: "and find in which projects I'm working on! (maybe none right now, at the moment).",
                 },
                 about:{
-                    title:  "A little about me",
-                    text1:  "Wanting to work in this virtual world that is the internet begins with being a videogames player. " +
+                    title1:  "A little about me",
+                    title2:  "Education",
+                    title3:  "Experience",
+                    personal:  "Wanting to work in this virtual world that is the internet begins with being a videogames player. " +
                             "Videogames have always caught my attention and I wanted to enter the world of programming because of them." + "\n\n" +
                             "Right now I am studying <b>web development</b>, identifying myself as a full-stack developer. " + 
                             "Javascript is my <b>main stack</b>, but I also have knowledge of other languages ​​and I would like to continue learning different technologies" + "\n\n" +
                             "My intention is to <b>continue learning as much as I can</b>, whether in a work environment, study or combining both. " +
                             "I consider myself an organized person, self-taught and with whom it is easy to work so, if you are <b>looking for someone for your team</b>, here I leave my contact information:",
+                    
                     section1Title: "Know my skills",
                     section1Text: "Here you will find which technologies I am in, which ones I have basic knowledge on and which ones make me curious to learn.",
                     section2Title: "Hobbies",
-                    section2Text: "<ul class='p-3'><li class='py-1'>Playing the guitar</li><li class='py-1'>Videogames</li><li class='py-1'>I love listening to music</li><li class='py-1'>To have ideas that I never develop</li></ul>",
-                }
+                    section2Text: "<ul class='p-3 mb-0'><li class='py-1'>Playing the guitar</li><li class='py-1'>Videogames</li><li class='py-1'>I love listening to music</li><li class='py-1'>To have ideas that I never develop</li></ul>",
+                },
             },
 
-            hola: true,
+            personalInformation: [
+                true, false, false
+            ],
+
+            education:[
+                { place: "IES La Sénia, Paiporta (Spain)", title: "Certificate of Higher Education (HNC)", type: "Software and Web development", date: "2019 - 2021" },
+                { place: "IES La Sénia, Paiporta (Spain)", title: "Vocational Education and Training (VET)", type: "IT & Computer Skills", date: "2018 - 2019" }
+            ],
+
+            experience:[
+                { place: "Ardis Software", title: "Training period", type: "Web & Angular developer", date: "Jan. - Jun. 2021" },
+                { place: "EMO - Especialistas en ortopedia", title: "Training period", type: "IT & PHP Programmer", date: "Mar. - Jun. 2019" }
+            ],
 
             buttons:{
                 tellMeMore: "Tell me more!",
                 bringMeThere: "Bring me there!",
                 takeALook: "Let's take a look",
+                about: "About me",
+                experience: "Education / Experience",
+                contact: "Contact",
                 contactMe: "Contact info"
             },
 
@@ -123,6 +142,27 @@ const vueApp = {
             
         },
 
+        showPersonalInfo( index ){
+
+            this.personalInformation.fill(false)
+            this.personalInformation[ index ] = true;
+            switch (index) {
+                case 0:
+                    this.content.about.title = "A little about me"
+                    break;
+                    
+                case 1:
+                    this.content.about.title = "Education"
+                    break;
+                    
+                case 2:
+                    this.content.about.title = "Experience"
+                    break;
+            }
+
+            console.log( this.personalInformation );
+        },
+
         copyText( event ){
 
             console.log( event.target.innerText );
@@ -134,76 +174,6 @@ const vueApp = {
 
             /* Copy the text inside the text field */
             document.execCommand("copy", false, text );
-        },
-
-        // Animate a delimiter or another based on chosen section
-        animateSeparator( section ){
-
-            // i to reduce, j to rise
-            let i = 120;
-            let j = 0;
-        
-            // Delay
-            setTimeout(function () {
-
-                // If we choose any section instead of 'home'
-                if( section != 0){
-
-                    // Select the right separator and reduce its height in a loop
-                    let interval = setInterval(function () {
-    
-                        this.separators[section - 1].style.height = i + "px";
-                        i--;
-                
-                        // Clear interval when separator its gone
-                        if (i < 0) clearInterval(interval);
-    
-                    }.bind(this), 9);
-                }
-
-                // If we choose 'home'
-                else{
-
-                    // We use x because 'section' value is not useful here
-                    let x = 0;
-                    if( this.projectsSection ) x = 1;
-
-                    let interval = setInterval(function () {
-
-                        this.separators[ x ].style.height = j + "px";
-                        j++;
-
-                        if (j > 120) clearInterval(interval);
-
-                    }.bind(this), 10);
-                }
-
-            }.bind(this), 200);
-            
-            
-
-        },
-
-        // When any inner section is toggled, deactivate every link
-        // and active just the one selected
-        activeInfo( section, index ){
-
-            this.aboutLinks.forEach(link => {
-                link.active = false;
-            });
-
-            switch ( section ) {
-                case 'about':
-                    
-                    this.aboutLinks[ index ].active = true;
-                    break;
-            
-                default:
-                    break;
-            }
-
-
-
         },
 
     },
